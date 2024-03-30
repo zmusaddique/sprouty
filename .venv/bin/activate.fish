321278344eb2173b302be4604e0f3e3d1b367612
Zmusaddique <musaddique092@gmail.com>
2024-03-30 17:15:42 +0530

    Update env packages

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/test/Desktop/FarmerBot/.venv"
+set -gx VIRTUAL_ENV "/home/test/Desktop/ML/Assignments/FarmerBot/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
